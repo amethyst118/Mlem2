249ab0bb05fb76135c4983caeb4f286eb33527db
Zuza <zuzkamoro@gmail.com>
2026-06-16 18:58:13 +0200

    update zad2 ze switchem

--- Zima2025C/Assets/SidePanel/SidePanelController.cs
+++ Zima2025C/Assets/SidePanel/SidePanelController.cs
@@ -1,9 +1,8 @@
-using System.Linq;
 using UnityEngine;
 
 public class SidePanelController : MonoBehaviour
 {
-    public enum SidePanelState
+    public enum SidePanelState                                  //definiuje wszystkie stany SidePanel
     {
         Minimized,
         MaximizedA,
@@ -12,88 +11,66 @@ public class SidePanelController : MonoBehaviour
     }
 
     public Animator animatorRef;
+    public SidePanelState currentSidePanelState = SidePanelState.Minimized;     //ustawia początkowy stan SidePanel jako minimized
 
-    public SidePanelState currentSidePanelState = SidePanelState.Minimized;
-
-    void Start()
+    public void OnSidePanelButtonClickA()                       //kiedy przycisk A jest kliknięty to zostaje ust. stan A, animator ustawiony na IsMaximizedA i inne panele się wyłączają
     {
-
+        HandlePanelClick
+        (
+            SidePanelState.MaximizedA,
+            "IsMaximizedA",
+            "IsMaximizedB",
+            "IsMaximizedC"
+        );
     }
 
-    public void OnSidePanelButtonClickA()
+    public void OnSidePanelButtonClickB()                       //kiedy przycisk B jest kliknięty to zostaje ust. stan B, animator ustawiony na IsMaximizedB i inne panele się wyłączają
     {
-        if (currentSidePanelState == SidePanelState.MaximizedA)
-        {
-            animatorRef.SetBool("IsMaximizedA", false);
-            currentSidePanelState = SidePanelState.Minimized;
-        }
-        else if(currentSidePanelState == SidePanelState.Minimized)
-        {
-            animatorRef.SetBool("IsMaximizedA", true);
-            currentSidePanelState = SidePanelState.MaximizedA;
-        }
-        else if (currentSidePanelState == SidePanelState.MaximizedB)
-        {
-            animatorRef.SetBool("IsMaximizedB", false);
-            animatorRef.SetBool("IsMaximizedA", true);
-            currentSidePanelState = SidePanelState.MaximizedA;
-        }
-        else
-        {
-            animatorRef.SetBool("IsMaximizedC", false);
-            animatorRef.SetBool("IsMaximizedA", true);
-            currentSidePanelState = SidePanelState.MaximizedA;
-        }
-    
+        HandlePanelClick
+        (
+            SidePanelState.MaximizedB,
+            "IsMaximizedB",
+            "IsMaximizedA",
+            "IsMaximizedC"
+        );
     }
 
-    public void OnSidePanelButtonClickB()
+    public void OnSidePanelButtonClickC()                     //kiedy przycisk C jest kliknięty to zostaje ust. stan C, animator ustawiony na IsMaximizedC i inne panele się wyłączają
     {
-       if (currentSidePanelState == SidePanelState.MaximizedB){
-            animatorRef.SetBool("IsMaximizedB", false);
-            currentSidePanelState = SidePanelState.Minimized;
-       }
-        else if(currentSidePanelState == SidePanelState.Minimized)
-        {
-            animatorRef.SetBool("IsMaximizedB", true);
-            currentSidePanelState = SidePanelState.MaximizedB;
-        }
-        else if (currentSidePanelState == SidePanelState.MaximizedA)
-        {
-            animatorRef.SetBool("IsMaximizedA", false);
-            animatorRef.SetBool("IsMaximizedB", true);
-            currentSidePanelState = SidePanelState.MaximizedB;
-        }
-        else
-        {
-            animatorRef.SetBool("IsMaximizedC", false);
-            animatorRef.SetBool("IsMaximizedB", true);
-            currentSidePanelState = SidePanelState.MaximizedB;
-        }
-    }
-    public void OnSidePanelButtonClickC()
-    {
-      if (currentSidePanelState == SidePanelState.MaximizedC){
-            animatorRef.SetBool("IsMaximizedC", false);
-            currentSidePanelState = SidePanelState.Minimized;
-    }
-        else if(currentSidePanelState == SidePanelState.Minimized)
-        {
-            animatorRef.SetBool("IsMaximizedC", true);
-            currentSidePanelState = SidePanelState.MaximizedC;
-        }
-        else if (currentSidePanelState == SidePanelState.MaximizedB)
-        {
-            animatorRef.SetBool("IsMaximizedB", false);
-            animatorRef.SetBool("IsMaximizedC", true);
-            currentSidePanelState = SidePanelState.MaximizedC;
-        }
-        else
-        {
-            animatorRef.SetBool("IsMaximizedA", false);
-            animatorRef.SetBool("IsMaximizedC", true);
-            currentSidePanelState = SidePanelState.MaximizedC;
-        }
+        HandlePanelClick
+        (
+            SidePanelState.MaximizedC,
+            "IsMaximizedC",
+            "IsMaximizedA",
+            "IsMaximizedB"
+        );
     }
 
+    private void HandlePanelClick(SidePanelState targetState, string selected, string otherA, string otherB)
+    {
+        switch (currentSidePanelState)                       //w zależności od otwartego panelu wykonuje się inna funkcja
+        {
+            case SidePanelState.Minimized:                   // w tym wypadku jeśli panel jest zamknięty to otwiera się wybrany przez nas panel i jest update animatora i stanu
+                animatorRef.SetBool(selected, true);
+                currentSidePanelState = targetState;
+                break;
+
+            case SidePanelState.MaximizedA:
+            case SidePanelState.MaximizedB:
+            case SidePanelState.MaximizedC:                 //tu jest wypadek kiedy jakikolwiek panel jest otwarty
+                if (currentSidePanelState == targetState)   //tu jeśli klika się otwarty panel do zamknięcia
+                {
+                    animatorRef.SetBool(selected, false);
+                    currentSidePanelState = SidePanelState.Minimized;
+                }
+                else                                        //tu jeśli inny panel jest otwarty to zamyka się poprzedni i otwiera się wybrany inny panel
+                {
+                    animatorRef.SetBool(otherA, false);
+                    animatorRef.SetBool(otherB, false);
+                    animatorRef.SetBool(selected, true);
+                    currentSidePanelState = targetState;
+                }
+                break;
+        }
+    }
 }
